cb93da3dc38ace912dbaa4c44270795b551b69d7
jbenjaminy <young.benjamin@gmail.com>
2017-01-15 18:34:08 -0600

    adds content to data-types playground

--- 1.3_data-types.playground/Contents.swift
+++ 1.3_data-types.playground/Contents.swift
@@ -1,5 +1,26 @@
 // DATA TYPES
 
-    // Declare a constant with the "let" keyword and a variable with the "var" keyword
-    
+    // Declare a constant with the "let" keyword
+    var playersCurrentlyOnline = 3
+    var myAge = 27
+    var myWeight = 165.5
+
+    // Declare a variable with the "var" keyword
+    let PI = 3.141592654
+    let stateOfBirth = "New York"
+    let yearMilesDavisWasBorn = 1926
+
+    // String: Words or sentences (a sequential series of characters), e.g., "There never seems to be enough time and people to do the paperwork."
+    // Int: An integer or whole number, e.g., 1967.
+    // Double: A number with a fractional component, e.g., 98.6.
+    // Bool: Either true or false.
+
+    // Fitness app exercise:
+    let name = "Ben"
+    let sex = "male"
+    var location = "Texas"
+    var weight = 165.5
+    var milesRunToday = 3
+    var numberOfPushups = 50
+    var didWorkOutToday = true
 
